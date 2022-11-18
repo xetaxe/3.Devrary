db6790191fdccb8b29c46db562bab5c3cad662de
nil <xetaxe@gmail.com>
2022-11-18 21:32:36 +0100

    Realize by watching tutorials that app structure is not good, two npms ar needed... delete soon

--- server.ts
+++ server.ts
@@ -1,12 +1,24 @@
 import express from 'express'
 import React from 'react'
 const app = express()
-const port = 3000
+const PORT = 3000;
 
-app.get('/', (req:any, res:any) => {
-  res.send('Hello World!')
-})
+import homeController from './src/home/homeController';
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+// app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.set('view engine', 'html');
+
+app.use('/test', express.static('public'));
+app.set
+
+app.use('/', homeController);
+
+
+// app.get('/', (req:any, res:any) => {
+//   res.send('Hello World!')
+// })
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Example app listening on port http://localhost:${PORT}/`)
 })
